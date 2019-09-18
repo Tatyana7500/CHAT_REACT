b89d8a90ab30f68ab60bb7d77104d74628a6c097
Your Name <you@example.com>
2019-09-18 05:11:10 +0300

    initial project structure

---
+++ src/client/loginComponent/LoginComponent.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class LoginComponent extends Component{
+    render(){
+        return <h2>Страница для входа</h2>;
+    }
+}
---
+++ src/client/signinComponent/SigninComponent.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class SigninComponent extends Component{
+    render(){
+        return <h2>Страница для регистрации</h2>;
+    }
+}
